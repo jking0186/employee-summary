594e46e27ea8241746cfb5a9e07c18baf72bf9fc
Joe King <jking0186@gmail.com>
2020-06-17 21:38:51 -0500

    inputs and outputs

--- Develop/app.js
+++ Develop/app.js
@@ -14,11 +14,11 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-    const employeeQuestions = [
+const employeeQuestions = [
     {
         type: "input",
         name: "name",
-        message:  "What is the employee's name?"
+        message: "What is the employee's name?"
     },
     {
         type: "input",
@@ -38,52 +38,119 @@ const render = require("./lib/htmlRenderer");
             "Manager",
             "Engineer",
             "Intern"
-        ]}
+        ]
+    }
 ];
-    const managerQuestions = [
-        {
-            type: "input",
-            name: "officeNumber",
-            message: "What is the manager's office number?"
-        }
-    ]
-    const engineerQuestions = [
-        {
-            type: "input",
-            name: "githubUserName",
-            message: "What is the engineer's github username?"
-        }
-    ]
-    const internQuestions = [
-        {
-            type: "input",
-            name: "schoolName",
-            message: "What school does the intern attend?"
-        }
-    ]
-async function init(){
-    try{
-        // ask basic questions first
-        await inquirer.prompt(employeeQuestions).then(function(response) {
+const managerQuestions = [
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?"
+    }
+]
+const engineerQuestions = [
+    {
+        type: "input",
+        name: "githubUserName",
+        message: "What is the engineer's github username?"
+    }
+]
+const internQuestions = [
+    {
+        type: "input",
+        name: "schoolName",
+        message: "What school does the intern attend?"
+    }
+]
+
+const addAnother = [
+    {
+        type: "list",
+        name: "addAnother",
+        message: "Would you like to add another team member?",
+        choices: [
+            "Yes",
+            "No"
+        ]
+    }
+]
+
+const employeeArray = [];
+let employee = "";
+
+async function init() {
+    try {
+        // ask employee questions first
+        await inquirer.prompt(employeeQuestions).then(function (response) {
             return teamData = response;
         });
-    }
-}
 
-// function call to initialize program
-init();
+// filter out the role response here
+switch (employeeData.role) {
+        case "Manager":
+            await inquirer.prompt(managerQuestion).then(function (response) {
+                employeeData.officeNumber = response.officeNumber;
+            });
+
+            employee = new Manager(employeeData.name, employeeData.id, employeeData.email, employeeData.officeNumber);
+
+            break;
+
+        case "Engineer":
+            await inquirer.prompt(engineerQuestion).then(function (response) {
+                employeeData.github = response.githubUserName;
+            });
+
+            employee = new Engineer(employeeData.name, employeeData.id, employeeData.email, employeeData.github);
+
+            break;
+
+        case "Intern":
+            await inquirer.prompt(internQuestion).then(function (response) {
+                employeeData.school = response.schoolName;
+            });
+
+            employee = new Intern(employeeData.name, employeeData.id, employeeData.email, employeeData.school);
+
+            break;
+
+        default:
+
+            break;
+    };
+
+
+employeeArray.push(employee);
+console.log("Employee added!");
+
+await inquirer.prompt(addAnother).then(function(response){
+    return decision = response.addAnother;
+});
+
+// if decision above is "yes" run the async function again
+if(decision === "Yes"){
+    await userData();
+} else {
+    let allEmployees = render(employeeArray);
+
+    
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function(err){
-        if(err){
-            console.log(err);
-        }
-            console.log("Success!");
-    });
+fs.writeFile(outputPath, allEmployees, function(err){
+    if(err){
+        console.log(err);
+    }
+        console.log("Data entered!");
+})   
 }
+
+} catch (err){
+console.log(err);
+}}
+// function call to initialize program
+init();
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
@@ -100,3 +167,4 @@ function writeToFile(fileName, data) {
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
